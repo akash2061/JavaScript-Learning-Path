077018141dfbbf99c888d5523063c02b3502a4b1
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-07 23:27:44 +0530

    Game-Update

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,5 +3,6 @@ let moves = document.querySelectorAll(".sbox");
 moves.forEach(move => {
     move.addEventListener("click", () => {
         alert("Game is In Progress...!");
+        move.innerText="Help...? :D";
     });
 });
