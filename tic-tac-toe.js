82b22004ac2f6676b904e346c7013e6b8b16ef8c
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-08 19:47:20 +0530

    Mode

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,3 +6,18 @@ moves.forEach(move => {
         move.innerText="Help...? :D";
     });
 });
+
+let modebtn = document.querySelector("#c_btw");
+let body = document.querySelector("body");
+let cmode = "light";
+modebtn.addEventListener("click", () => {
+    if (cmode === "light") {
+        cmode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else {
+        cmode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+});
