d636c7779c1e1db4ae32589c64cf2b4225516e0e
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-08 21:25:29 +0530

    Update

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,44 @@
 let moves = document.querySelectorAll(".sbox");
+let reset = document.querySelectorAll("#rb");
 
+let turn = true;
+let win = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+];
 moves.forEach(move => {
     move.addEventListener("click", () => {
-        alert("Game is In Progress...!");
-        move.innerText="Help...? :D";
+        // alert("Game is In Progress...!");
+        if (turn) {
+            move.innerText = "O";
+            turn = false;
+        } else {
+            move.innerText = "X";
+            turn = true;
+        }
+        move.disabled = true;
+        checkwin();
     });
 });
+function checkwin() {
+    for (let p of win){
+        let p1 = moves[p[0]].innerText;
+        let p2 = moves[p[1]].innerText;
+        let p3 = moves[p[2]].innerText;
 
+        if(p1 != "" && p2 != "" && p3 != ""){
+            if(p1===p2 && p2===p3){
+                
+            }
+        }
+    }
+};
 let modebtn = document.querySelector("#c_btw");
 let body = document.querySelector("body");
 let cmode = "light";
