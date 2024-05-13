4dd196e16621bcc328076e3ce6ba885142b09bd0
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-13 20:05:10 +0530

    Code

---
+++ async.js
@@ -0,0 +1,10 @@
+console.log("1");
+console.log("2");
+setTimeout(() => {
+    console.log("Hello...");
+}, 2000);
+console.log("3");
+console.log("4");
+setTimeout(() => {
+    console.log("Hello...!");
+}, 2000);
