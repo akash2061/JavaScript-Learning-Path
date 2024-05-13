04ce225550f103c49d22542fc814d7487615501b
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-13 20:33:35 +0530

    Code

--- async.js
+++ async.js
@@ -2,9 +2,19 @@ console.log("1");
 console.log("2");
 setTimeout(() => {
     console.log("Hello...");
-}, 2000);
+}, 1000);
 console.log("3");
 console.log("4");
 setTimeout(() => {
     console.log("Hello...!");
-}, 2000);
+}, 1000);
+
+function getData(dataId) {
+    setTimeout(() => {
+        console.log("Data: ", dataId);
+    }, 2000);
+}
+
+getData(1);
+getData(2);
+getData(3);
